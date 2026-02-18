00522105cb49209420749a0ecd6da6336fb0dac3
jghimire1 <157061368+jghimire1@users.noreply.github.com>
2026-02-18 12:05:58 -0700

    Remove date from combo_menu.py comments

--- combo_menu.py
+++ combo_menu.py
@@ -1,7 +1,5 @@
-# Project: Assignment set 7 - module combo menu with Enum 
 # Description: Create an Enum class for a combo menu
 # Author: Janardan Ghimire
-# Date: 12/11/2025
 
 
 from enum import Enum
@@ -11,4 +9,5 @@ class Combo_Menu(Enum):
     CANIAC = 2
     FINGERS = 3
     SANDWICH = 4
+
     KIDS = 5 
